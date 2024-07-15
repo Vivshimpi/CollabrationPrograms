d0056079e7e3bb1d28c7042bfdae386b6ab7dce2
Acer <Acer@DESKTOP-4MF4EAG>
2024-07-15 17:37:32 +0530

    "2nd largest number in Array"

--- src/test/java/chetan/secondlargestinarray.java
+++ src/test/java/chetan/secondlargestinarray.java
@@ -8,15 +8,15 @@ public class secondlargestinarray {
 	public static void main(String[] args) {
 		// JAVA PROGRAM TO FIND SECOUND LARGEST ELEMENT IN ARRAY
 		
-		int ar[]= {12,12,55,88,66,88,44,755,6666,888,87988};
+		int ar[]= {12,12,55,88,66,88,44,755,6666,87988,888,87988};
 		TreeSet tr=new TreeSet();
 		for(int num:ar)
 		{
 			tr.add(num); // got Ascending order without duplicate num 12,44,55,88,755,888,6666,87988
 		}
-		 ArrayList al=new ArrayList(tr);
+		 ArrayList al=new ArrayList(tr); // convert into 
 		
-		 System.out.println("2nd largest number in Array:"+ al.get(7));
+		 System.out.println("2nd largest number in Array:"+ al.get(al.size()-2));
 		 
 	}
 
