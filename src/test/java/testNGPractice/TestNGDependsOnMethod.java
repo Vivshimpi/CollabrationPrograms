1f1569de1efa541d3d6f840c7b6f4ca79a3ab3e2
Vivshimpi <viv.shimpi09@gmail.com>
2024-07-18 17:04:56 +0530

    "vivek = add DataProvider"

--- src/test/java/testNGPractice/TestNGDependsOnMethod.java
+++ src/test/java/testNGPractice/TestNGDependsOnMethod.java
@@ -5,34 +5,34 @@ import org.testng.annotations.Test;
 
 public class TestNGDependsOnMethod 
 {
-	@Test(priority=1)
+	@Test(priority=1, invocationCount = 2)
 	void openapp()
 	{
-      Assert.assertEquals(1,2); 
+      Assert.assertEquals(1,1); 
 	}
 
-	@Test(priority=2, dependsOnMethods = {"openapp"})
+	//@Test(priority=2, dependsOnMethods = {"openapp"}, alwaysRun = true)
 	void login()
 	{
 	      Assert.assertTrue(true);
 
 	}
 
-	@Test(priority=3,dependsOnMethods = {"login"})
+	//@Test(priority=3,dependsOnMethods = {"login"})
 	void search()
 	{
 	      Assert.assertTrue(true);
 
 	}
 	
-	@Test(priority=4,dependsOnMethods = {"login", "search"})
+	//@Test(priority=4,dependsOnMethods = {"login", "search"})
 	void advSearch()
 	{
 	      Assert.assertTrue(true);
 
 	}
 
-	@Test(priority=5)
+	//@Test(priority=5)
 	void logout()
 	{
 	      Assert.assertTrue(true);
