95327e1db246e0effaed025467b4835da721733f
vishimpi <vivekshimpi027@gmail.com>
2024-07-22 17:51:10 +0530

    "vivek - add leap year, remove special char"

---
+++ src/test/java/vivek/RemoveSpecialChar.java
@@ -0,0 +1,13 @@
+package vivek;
+
+public class RemoveSpecialChar
+{
+public static void main(String[] args) {
+	String s = "this is @ me";
+    //op=this is me
+	
+	String str= s.replaceAll("\\W+", " ");
+	System.out.println(str);
+	
+}
+}
